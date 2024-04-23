28fd4ce1a1d2c7ade57432eb06f69567188113df
Linhkobe <linh001200000101@gmail.com>
2024-04-23 18:43:07 +0200

    App Interface

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-# Crossword
+# Front-end with Kivy
 
-## Les principes de l'application
-1) L'utilisateur peut prendre un puzzle en photo
-2) Le système détecte le mot croiser dans la photo
-3) L'utilisateur prends les phrase en photo 
-4) le système détecte l’indice  dans la photo
-5) Le système convertit les indice en prompt pour l’ia
-6) L’ia reçoit les indices incis que les info sur le mot croisé
-7) L’ia génère des réponse 
-8) Le système reçoit les réponses et remplis les mot croiser 
+## 1.Objective : 
+The App is a simple mobile application developed using KivyMD for capturing images of crossword and its clues through the device's camera. 
+
+## 2.Features : 
+
++ Welcome Screen with a "Get Start" button to navigate to the main camera screen.
++ Camera Screen for capturing images.
++ Image preview after capture.
++ Navigation between screens.
++ Responsive layout for different screen sizes.
+
+## 3.Installations 
+
+- To install kivy : pip install kivy
+- To install kivymd : pip install kivymd
+- To install buildozer : pip install buildozer
+
+## 4.Requirements : 
+
+    Python 3.6 or later
+    Kivy 2.0.0 or later
+    KivyMD 0.104.2 or later
+
+## 5.How to run this ? 
+ just need to run the file "main.py" and the interface of the app will apeear. 
---
+++ main.py
@@ -0,0 +1,101 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
+from kivy.core.window import Window
+from kivy.uix.image import Image
+from kivymd.uix.list import MDList,OneLineAvatarListItem,ImageLeftWidget
+from kivy.uix.camera import Camera
+import time
+import os
+import subprocess
+from kivy.config import Config
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+
+Window.size = (450,900)
+
+Builder.load_file("homescreen.kv")
+
+class WelcomeScreen(Screen):
+    pass
+
+class RotatingCamera(Camera):
+    def on_texture(self, instance, texture):
+        # Rotate the texture by 90 degrees clockwise
+        if texture:
+            img = PilImage.frombytes('RGBA', texture.size, texture.pixels)
+            img = img.rotate(-90, expand=True)
+            buf = img.tobytes()
+            texture = Texture.create(size=img.size, colorfmt='rgba')
+            texture.blit_buffer(buf, colorfmt='rgba', bufferfmt='ubyte')
+            self.texture = texture
+
+class Homescreen(MDScreen):
+	def __init__(self,**kwargs):
+		super().__init__(**kwargs)
+		 
+		self.mycamera = self.ids.camera
+		self.myimage = Image()
+		self.resultbox = self.ids.resultbox
+		self.mybox = self.ids.mybox
+  
+		# Join files 
+		# self.image_folder = os.path.join(os.getcwd(), "kivy_test", "kivymdCamera")
+		# self.backend_folder = os.path.join(os.getcwd(), "kivy_test", "backend")
+
+
+	def captureyouface(self):
+		# CREATE TIMESTAMP NOT FOR YOU FILE IMAGE
+		timenow = time.strftime("%Y%m%d_%H%M%S")
+		# timenow = time.strftime("%Y%m%d_%H%M%S")
+
+		# # AND EXPORT YOU CAMERA CAPTURE TO PNG IMAGE
+		self.mycamera.export_to_png("myimage_{}.png".format(timenow))
+		# timenow = time.strftime("%Y%m%d_%H%M%S")
+		# image_path = os.path.join(self.image_folder, "myimage_{}.png".format(timenow))
+
+        # # Export image
+		# self.mycamera.export_to_png(image_path)
+
+        # # Call backend scripts
+		# subprocess.run(["python", os.path.join(self.backend_folder, "detectionDeGrilleCW.py"), image_path])
+		# subprocess.run(["python", os.path.join(self.backend_folder, "main_definition_ocr.py"), image_path]) 
+		self.myimage.source = "myimage_{}.png".format(timenow)
+		self.resultbox.add_widget(
+			OneLineAvatarListItem(
+				ImageLeftWidget(
+					source="myimage_{}.png".format(timenow),
+					size_hint_x=0.3,
+					size_hint_y=1,
+
+					# AND SET YOU WIDHT AND HEIGT YOU PHOTO
+					size=(300,300)
+
+					)
+    			# 	,
+				# text=self.ids.name.text
+				)
+
+			)
+  
+class CrosswordApp(MDApp):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(WelcomeScreen(name='welcome'))
+        sm.add_widget(Homescreen(name='home'))
+        return sm
+
+CrosswordApp().run()
+	
+# class CrosswordApp(MDApp):
+# 	def build(self):
+# 		return Homescreen()
+
+# if __name__ == "__main__":
+# 	CrosswordApp().run()
+ 
+ 
+
